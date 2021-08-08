44254d94cd08889c2a329f43f9c44274e8da5ef8
GaryLuoLuo <2garylocke@gmail.com>
2021-08-08 10:36:44 -0700

    no message

--- projects/rmdb/src/hooks/useUser.js
+++ projects/rmdb/src/hooks/useUser.js
@@ -22,3 +22,13 @@ export const useUser = () => {
 
   return {user, signIn, signOut}
 }
+
+// TODO:
+/**
+Your useAuth should encapsulate the following knowledge:
+
+Whether the user is authenticated
+Who the user is
+Setting / unsetting the authenticated user
+Verifying the username/password credentials are valid
+**/
